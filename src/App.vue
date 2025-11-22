a399babcfd4f1026af52f99361871b87df73a350
amirsafaricg <a.safari1380@gmail.com>
2025-11-22 18:01:09 +0330

    installing necessary tools

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="h-dvh w-dvw">
+    <CalendarGrid :days="4" />
+  </div>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import CalendarGrid from './components/CalendarGrid.vue'
 
+export default defineComponent({
+  name: 'AppView',
+  components: {
+    CalendarGrid,
+  },
+  setup() {},
+})
+</script>
 <style scoped></style>
---
+++ src/components/CalendarGrid.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="w-full overflow-auto dope-calendar-grid h-full">
+    <div class=""></div>
+  </div>
+</template>
+<script lang="ts">
+import { type PropType, defineComponent, onMounted } from 'vue'
+export default defineComponent({
+  name: 'CalendarGrid',
+  props: {
+    georgian: {
+      type: Boolean,
+      default: true,
+    },
+    jalaali: {
+      type: Boolean,
+      default: false,
+    },
+    date: {
+      type: Date,
+      required: true,
+      default: new Date(),
+    },
+    days: {
+      type: Number,
+      default: 'auto',
+    },
+  },
+  setup(props) {
+    onMounted(() => {
+      console.log(props.days)
+    })
+  },
+})
+</script>
+<style scoped>
+@import '@/assets/css/calendar.css';
+</style>
